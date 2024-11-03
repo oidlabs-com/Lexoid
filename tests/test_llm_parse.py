bebbc63d2f49673e2ca271401ff988672f65066d
Pramit Choudhary <pchoudha@uci.edu>
2024-11-03 13:38:17 -0800

    Make unit tests async #18

--- tests/test_llm_parse.py
+++ tests/test_llm_parse.py
@@ -1,9 +1,11 @@
 # python3 -m pytest tests/test_llm_parse.py -v
 # With logs: python3 -m pytest tests/test_llm_parse.py -v -s
 
+import os
+
 import pytest
 from dotenv import load_dotenv
-import os
+
 from lexoid.api import parse
 from lexoid.core.utils import calculate_similarity
 
@@ -12,11 +14,12 @@ output_dir = "tests/outputs"
 os.makedirs(output_dir, exist_ok=True)
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize(
     "models",
     ["gpt-4o", "gpt-4o-mini", "gemini-1.5-flash", "gemini-1.5-pro"],
 )
-def test_llm_parse(models):
+async def test_llm_parse(models):
     input_data = "examples/inputs/test_1.pdf"
     expected_ouput_path = "examples/outputs/test_1.md"
     config = {"parser_type": "LLM_PARSE", "model": models, "verbose": True}
@@ -27,6 +30,27 @@ def test_llm_parse(models):
     expected_ouput = open(expected_ouput_path, "r").read()
     # save the result to a file
     with open(f"{output_dir}/input_table_{models}.md", "w") as f:
+        f.write(result)
+    score = calculate_similarity(result, expected_ouput)
+    assert round(score, 3) > 0.8
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "models",
+    ["gpt-4o", "gpt-4o-mini", "gemini-1.5-flash", "gemini-1.5-pro"],
+)
+async def test_jpg_parse(models):
+    input_data = "examples/inputs/test_4.jpg"
+    expected_ouput_path = "examples/outputs/test_4.md"
+    config = {"parser_type": "LLM_PARSE", "model": "gpt-4o"}
+    result = parse(input_data, raw=True, **config)
+    assert isinstance(result, str)
+
+    # Compare the result with the expected output
+    expected_ouput = open(expected_ouput_path, "r").read()
+    # save the result to a file
+    with open(f"{output_dir}/input_image_{models}.md", "w") as f:
         f.write(result)
     score = calculate_similarity(result, expected_ouput)
     assert round(score, 3) > 0.8
